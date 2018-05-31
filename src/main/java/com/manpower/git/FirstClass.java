9c3d3f9fdb3b8faaf849fc5065cc1f1e7691ec54
Christy Julia <tianaced@gmail.com>
2018-05-31 09:55:09 +0200

    My first git project

---
+++ src/main/java/com/manpower/git/FirstClass.java
@@ -0,0 +1,15 @@
+package com.manpower.git;
+
+
+
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Hellow World!");
+		System.out.println("Premier git class");
+		
+	}
+
+}
---
+++ src/test/java/com/manpower/gittest/TestFirstClass.java
@@ -0,0 +1,20 @@
+package com.manpower.gittest;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestFirstClass {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testFirstClass() {
+		
+	
+	}
+
+}
